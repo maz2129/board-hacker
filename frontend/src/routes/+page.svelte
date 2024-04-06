eaebe4d3835d2bc9a8ce75e22018afa14ea15db1
Matt Zinman <maz2129@columbia.edu>
2024-04-06 13:02:01 -0400

    backend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>NYC Manhattan Community Board 1</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Board Members</h2>
